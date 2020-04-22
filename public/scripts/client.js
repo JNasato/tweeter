cb7b2eae9fa2af3a61302e949e49909b76db26da
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-22 21:13:18 +0000

    set ajax get request so tweets loaded from database are rendered on webpage

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd"
-//     },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
-renderTweets(data);
-
 function loadPosts() {
   $.getJSON('/tweets')
     .then((tweets) => {
@@ -49,12 +21,12 @@ $('.new-tweet form').submit(function (event) {
     .then(() => {
       console.log(data);
       $(self)[0].reset();
-      // renderTweets(data);
+      $('#tweets-container').empty();
+      loadPosts();
     })
     .catch(err => console.log('Error: ', err))
 });
 
-
-
+loadPosts();
 
 
